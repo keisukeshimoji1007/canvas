088d735b9c7d124c81a793771d39808656d77ed8
keisukeshimoji1007 <keisukeshimoji1007@gmail.com>
2018-10-19 15:45:09 +0900

    practice canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'canvas';
+
+  constructor() {
+  }
+
+  square = {
+    x1: 10,
+    y1: 10,
+    x2: 100,
+    y2: 100,
+  }
+  ctx;
+
+  ngOnInit() {
+    const canvas = <HTMLCanvasElement> document.getElementById('canvas')
+    this.ctx = canvas.getContext('2d');
+    this.ctx.fillStyle = 'green';
+    this.ctx.fillRect(this.square.x1, this.square.y1, this.square.x2, this.square.y2);
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onmousemove(e) {
+    console.log(e.clientX, e.clientY);
+    this.ctx.clearRect(this.square.x1, this.square.y1, this.square.x2, this.square.y2);
+    this.square.x1 = e.clientX;
+    this.square.y1 = e.clientY;
+    this.ctx.fillRect(this.square.x1, this.square.y1, this.square.x2, this.square.y2);
+  }
 }
